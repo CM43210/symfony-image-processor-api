68756c8d36bbb396d2f66090ef351c0ef3920919
Cezary Marcinik <cezary.marcinik@o2.pl>
2025-12-04 21:11:13 +0100

    build(migrations): update migration paths and add image table migration

---
+++ src/Infrastructure/Migrations/Version20251204200926.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251204200926 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE image (id CHAR(36) NOT NULL, uploaded_at DATETIME NOT NULL, file_path VARCHAR(500) NOT NULL, file_format VARCHAR(10) NOT NULL, file_size_bytes INT NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE image');
+    }
+}
